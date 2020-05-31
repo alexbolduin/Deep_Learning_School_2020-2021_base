206d296100c246af70c4a7af9b31bdd5c80df0de
Alexander Chernyshev <achernys123@mail.ru>
2020-05-31 20:04:10 +0300

    code changed

--- parser.py
+++ parser.py
@@ -2,8 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = 'https://auto.ru/moskva/cars/ford/explorer/all/?sort=fresh_relevance_1-desc&geo_id=101064'
-HEADERS = {'user_agent': "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36",
-           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
+HEADERS = {'user_agent': "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
+                          Chrome/83.0.4103.61 Safari/537.36",
+           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,\
+                      application/signed-exchange;v=b3;q=0.9'}
 
 
 def get_html(url, params=None):
@@ -15,9 +17,11 @@ def get_content(html):
     items = soup.find_all('div', class_ = 'ListingItem-module__main')
 
     cars = []
+    rus = 'Ð\xa0ÐµÑ\x81Ñ\x82Ð°Ð¹Ð»Ð¸Ð½Ð³'
+    eng = 'Restailing'
     for item in items:
        cars.append({
-            'title': item.find('a', class_='Link ListingItemTitle-module__link').get_text(strip=True).replace('Ð\xa0ÐµÑ\x81Ñ\x82Ð°Ð¹Ð»Ð¸Ð½Ð³', 'Restailing'),
+            'title': item.find('a', class_='Link ListingItemTitle-module__link').get_text(strip=True).replace(rus, eng),
             'link': item.find('a', class_='Link ListingItemTitle-module__link').get('href')
 
        })

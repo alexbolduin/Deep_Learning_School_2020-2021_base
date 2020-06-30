6b40eaefe9c9232398c2a9acbe1b63dd9bd048fe
Alexander Chernyshev <achernys123@mail.ru>
2020-06-30 22:39:07 +0300

    code changed

--- parser.py
+++ parser.py
@@ -1,40 +1,34 @@
-import requests
+from selenium import webdriver
+from path import Path
 from bs4 import BeautifulSoup
 
-URL = 'https://auto.ru/moskva/cars/ford/explorer/all/?sort=fresh_relevance_1-desc&geo_id=101064'
-HEADERS = {'user_agent': "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
-                          Chrome/83.0.4103.61 Safari/537.36",
-           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,\
-                      application/signed-exchange;v=b3;q=0.9'}
+link_to_driver = Path('C:\chromedriver\chromedriver.exe')
+driver = webdriver.Chrome(link_to_driver)
 
+driver.get('https://www.flashscore.com/football/england/premier-league-1989-1990/results/')
 
-def get_html(url, params=None):
-    r = requests.get(url, headers=HEADERS, params=params)
-    return r
+elements = driver.find_elements_by_class_name('event__match--static')
 
-def get_content(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    items = soup.find_all('div', class_ = 'ListingItem-module__main')
+def get_table(elems):
+    table = []
+    for el in elems:
+        html = el.get_attribute('innerHTML')
+        soup = BeautifulSoup(html, 'html.parser')
+        date = soup.find('div', class_='event__time').get_text(strip=True)
+        home_team = soup.find('div', class_='event__participant--home').get_text(strip=True)
+        away_team = soup.find('div', class_='event__participant--away').get_text(strip=True)
+        score = soup.find('div', class_='event__scores').get_text(strip=True)
+        table.append({'date': date[:5].replace('.', '-'),
+                      'home_team': home_team,
+                      'away_team': away_team,
+                      'score': score})
 
-    cars = []
-    rus = 'Ð\xa0ÐµÑ\x81Ñ\x82Ð°Ð¹Ð»Ð¸Ð½Ð³'
-    eng = 'Restailing'
-    for item in items:
-       cars.append({
-            'title': item.find('a', class_='Link ListingItemTitle-module__link').get_text(strip=True).replace(rus, eng),
-            'link': item.find('a', class_='Link ListingItemTitle-module__link').get('href'),
-            'price': item.find('div', class_='ListingItemPrice-module__content').get_text(strip=True)
-
-       })
-
-    print(cars)
-    print(len(cars))
+    return table
 
 def parse():
-    html = get_html(URL)
-    if html.status_code == 200:
-        get_content(html.text)
-    else:
-        print(f'Error with status_code {html.status_code}')
+    table = get_table(elements)
+    print(table)
 
 parse()
+
+driver.close()

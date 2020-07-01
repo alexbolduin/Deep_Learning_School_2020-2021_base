eeaaf2872b6f5549cc004c1e16db05f0e4bc686a
Alexander Chernyshev <achernys123@mail.ru>
2020-07-01 15:41:10 +0300

    code changed

--- parser.py
+++ parser.py
@@ -1,15 +1,40 @@
-from selenium import webdriver
+from selenium import webdriver, common
 from path import Path
 from bs4 import BeautifulSoup
+import time
+import pandas as pd
 
-link_to_driver = Path('C:\chromedriver\chromedriver.exe')
-driver = webdriver.Chrome(link_to_driver)
 
-driver.get('https://www.flashscore.com/football/england/premier-league-1989-1990/results/')
+LINK_TO_DRIVER = Path('C:\chromedriver\chromedriver.exe')
+driver = webdriver.Chrome(LINK_TO_DRIVER)
+main_link = 'https://www.flashscore.com/football/england/premier-league-1989-1990/results/'
+main_link2 = 'https://www.flashscore.com/football/england/premier-league-2018-2019/results/'
 
-elements = driver.find_elements_by_class_name('event__match--static')
+driver.get(main_link)
+YEARS = driver.find_element_by_css_selector('div.teamHeader__text')
+years = YEARS.get_attribute('innerHTML').replace('/', '-')
+
+next_step = driver.find_elements_by_css_selector('a.event__more.event__more--static')
+next_step[-1].click()
+
+for i in range(1, 2, 1):
+    try:
+        next_step = driver.find_elements_by_css_selector('a.event__more.event__more--static')
+        next_step[-1].click()
+        print(len(next_step))
+        print('click ', i)
+        time.sleep(5)
+    except common.exceptions.ElementClickInterceptedException:
+        print('end')
+        break
+
+time.sleep(10)
+next_step = driver.find_elements_by_css_selector('a.event__more.event__more--static')
+next_step[-1].click()
+
 
 def get_table(elems):
+
     table = []
     for el in elems:
         html = el.get_attribute('innerHTML')
@@ -18,17 +43,32 @@ def get_table(elems):
         home_team = soup.find('div', class_='event__participant--home').get_text(strip=True)
         away_team = soup.find('div', class_='event__participant--away').get_text(strip=True)
         score = soup.find('div', class_='event__scores').get_text(strip=True)
-        table.append({'date': date[:5].replace('.', '-'),
+        table.append({'years': years,
+                      'date': date[:5].replace('.', '-'),
                       'home_team': home_team,
                       'away_team': away_team,
-                      'score': score})
+                      'score': score,
+                      'total': int(score[0]) + int(score[2])})
 
     return table
 
+
+driver.get(main_link)
+elements = driver.find_elements_by_css_selector('div.event__match.event__match--static.event__match--oneLine')
+
+
 def parse():
+
     table = get_table(elements)
+    print('Table len ', len(table))
+    print('Elements list len ', len(elements))
     print(table)
+    season_stat = pd.DataFrame(table)
+    season_stat.to_csv(f'season_{years}_statistic.csv')
+
 
 parse()
 
+print('parsing finished')
+time.sleep(20)
 driver.close()
